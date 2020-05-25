8de6695c8c083b7c4cf4bf6ea102880d9d06de2f
Sam <s.tandon130697@gmail.com>
2020-05-24 23:17:09 -0700

    Serializers0

---
+++ server/apps/endpoints/serializers.py
@@ -0,0 +1,42 @@
+from rest_framework import serializers
+from apps.endpoints.models import Endpoint
+from apps.endpoints.models import MlAlgorithm
+from apps.endpoints.models import MlAlgorithmStatus
+from apps.endpoints.models import MLRequest
+
+class EndpointSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Endpoint
+        read_only_fields = ("id","name","owner","created_at")
+        fields = read_only_fields
+
+class MlAlgorithmSerializer(serializers.ModelSerializer):
+
+    currentStatus = serializers.SerializerMethodField(read_only=True)
+
+    def getCurrentStatus(self, MlAlgorithm):
+        return MlAlgorithmStatus.objects.filter(parentAlgorithm = MlAlgorithm)\
+    .latest("createdAt").status
+
+    class Meta:
+        model = MlAlgorithm
+        read_only_fields = ("id", "name", "description", "code",
+                            "version", "owner", "createdAt", "parentEndpoint",
+                            "currentStatus")
+        fields = read_only_fields
+
+class MlAlgorithmStatusSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MlAlgorithmStatus
+        read_only_fields = ("id", "active")
+        fields = ("id", "active", "status", "createdBy",
+                  "createdAt", "parentAlgorithm")
+
+class MLRequestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MLRequest
+        read_only_fields = ("id", "inputData", "fullResponse","response",
+                            "feedback", "createdAt", "parentAlgorithm")
+
+
+
--- server/server/settings.py
+++ server/server/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps.endpoints'
+    'apps.endpoints',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
