6863f858a40b6845d03b003adb94f790eb7e1578
Sam <s.tandon130697@gmail.com>
2020-05-31 20:53:45 -0700

    Preprocessor3

--- server/apps/ml/preprocessors/textPreprocessor.py
+++ server/apps/ml/preprocessors/textPreprocessor.py
@@ -13,17 +13,17 @@ class textPreprocessor:
         CONTRACTION_MAPPING = json.load(jsonFile)
     COMPILED_RE = re.compile('(%s)' % '|'.join(CONTRACTION_MAPPING.keys()))
 
-    def lowerCase(self, string):
+    def lower_case(self, string):
         return string.lower()
 
-    def replaceWith(self, match):
+    def replace_with(self, match):
         return self.CONTRACTION_MAPPING[match.group(0)]
 
-    def expandContractions(self, string):
-        return self.COMPILED_RE.sub(self.replaceWith, string)
+    def expand_contractions(self, string):
+        return self.COMPILED_RE.sub(self.replace_with, string)
 
     def preprocess(self, string):
-        return ' '.join(preprocess_string(string, filters=[self.lowerCase, self.expandContractions, split_alphanum,
+        return ' '.join(preprocess_string(string, filters=[self.lower_case, self.expand_contractions, split_alphanum,
                                                    strip_multiple_whitespaces, strip_non_alphanum, strip_numeric,
                                                    strip_tags, strip_punctuation]))
 
